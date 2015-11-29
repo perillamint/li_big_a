a2e721d6c8de8ec000d0bc52d17f0bb5f7ca00db
Q <kmgluv0512@naver.com>
2015-11-29 17:03:20 +0900

    add verify

--- OTR_Module/verify.py
+++ OTR_Module/verify.py
@@ -2,7 +2,7 @@ import gnupg
 
 def Verify(msg, key):
 	gpg = gnupg.GPG(gnupghome="keys")
-	import_result = gpg.import_keys(key.data)
+	import_result = gpg.import_keys(key)
 	verify = gpg.verify(msg)
 	isVerified = verify.valid
 	return isVerified
